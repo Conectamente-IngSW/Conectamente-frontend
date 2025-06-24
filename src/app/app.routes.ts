43778541bfc2b7d8273872b69ed34f18fef0ae54
NicoleBances <nicolebances929@gmail.com>
2025-06-24 00:16:10 -0500

    Implementacion frontend login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+//Aquí se manejan todas las rutas del proyecto
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Carga del auth.routes, desde donde se manejan las rutas del login y registro
+    {
+        path: 'auth',
+        loadChildren: () => import('./pages/auth/auth.routes').then(m => m.authRoutes)
+    },
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    }
+];
