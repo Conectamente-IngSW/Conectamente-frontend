27cf5196c622fec809e352a691e73f00edf7cf86
NicoleBances <nicolebances929@gmail.com>
2025-06-23 22:20:36 -0500

    Organización inicial de carpetas

---
+++ src/app/pages/auth/auth-layout/auth-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.css']
+  //template: '<router-outlet></router-outlet>'
+})
+export class AuthLayoutComponent {
+
+}
