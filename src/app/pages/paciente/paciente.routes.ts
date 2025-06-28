625dfdc0f5ab6649063ccaf5b97e59bf7ff9c8e0
NicoleBances <nicolebances929@gmail.com>
2025-06-27 22:26:00 -0500

    perfil

--- src/app/pages/paciente/paciente.routes.ts
+++ src/app/pages/paciente/paciente.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ConfiguracionComponent } from './mi_cuenta/configuracion/configuracion.component';
 import { VisualizarCitaComponent } from './mi_cuenta/visualizar-cita/visualizar-cita.component';
 import { VisualizarPsicologoComponent } from './pantalla_principal/visualizar-psicologo/visualizar-psicologo.component';
+import { PerfilComponent } from './perfil_psicologo/perfil/perfil.component';
 
 export const pacienteRoutes: Routes = [
   {
@@ -9,7 +10,8 @@ export const pacienteRoutes: Routes = [
     children: [
       { path: 'mi_cuenta/configuracion', component: ConfiguracionComponent },
       { path: 'mi_cuenta/visualizar-cita', component: VisualizarCitaComponent },
-      {path: 'pantalla_principal/visualizar-psicologo', component: VisualizarPsicologoComponent }
+      {path: 'pantalla_principal/visualizar-psicologo', component: VisualizarPsicologoComponent},
+      {path: 'perfil_psicologo/perfil', component: PerfilComponent}    
     ]
   }
 ];
